7d286799d482a87f0e6a28aed722f418a596d03f
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-05-10 20:09:30 +0530

    az data source fix

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ module "public_subnet" {
   build_name = var.build_name
   subnet_cidr = "12.0.16.0/20"
   subnetname = "public"
-  count = length(data.aws_availability_zones.azs.names)
-  subnet_az = data.aws_availability_zones.azs.names[count.index]
+  #count = length(data.aws_availability_zones.azs.names)
+  subnet_az = data.aws_availability_zones.azs.names[0]
 }
 
 module "private_subnet" {
@@ -21,7 +21,7 @@ module "private_subnet" {
   subnet_cidr = "12.0.32.0/20"
   build_name = var.build_name
   subnetname = "private"
-  count = length(data.aws_availability_zones.azs.names)
-  subnet_az = data.aws_availability_zones.azs.names[count.index]
+  #count = length(data.aws_availability_zones.azs.names)
+  subnet_az = data.aws_availability_zones.azs.names[1]
   map_public_ip = false
 }
