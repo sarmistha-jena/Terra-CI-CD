eca99345f340e9304a9bcd722d6b7f8b2ec50332
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-05-10 19:58:21 +0530

    az data source fix

--- datasource.tf
+++ datasource.tf
@@ -1,17 +1,3 @@
 data "aws_availability_zones" "azs" {
   state = "available"
 }
-
-data "aws_subnets" "filtered_public" {
-  for_each = toset(data.aws_availability_zones.azs.zone_ids)
-
-  filter {
-    name   = "tag-key"
-    values = ["public"]
-  }
-
-  filter {
-    name   = "availability-zone-id"
-    values = ["${each.value}"]
-  }
-}
--- main.tf
+++ main.tf
@@ -5,19 +5,23 @@ module "vpc" {
 
 module "public_subnet" {
   source = "./modules/subnet"
-  subnet_az = [for k, v in data.aws_subnets.filtered_public : v.ids[0]]
   #subnet_az = data.aws_availability_zones.azs.id[0]
   vpc_id = module.vpc.vpc_id
   build_name = var.build_name
   subnet_cidr = "12.0.16.0/20"
   subnetname = "public"
+  count = length(data.aws_availability_zones.azs.names)
+  subnet_az = data.aws_availability_zones.azs.names[count.index]
 }
 
 module "private_subnet" {
   source = "./modules/subnet"
-  subnet_az = [for k, v in data.aws_subnets.filtered_public : v.ids[1]]
+  #subnet_az = [for k, v in data.aws_subnets.filtered_public : v.ids[1]]
   vpc_id = module.vpc.vpc_id
   subnet_cidr = "12.0.32.0/20"
   build_name = var.build_name
   subnetname = "private"
+  map_public_ip_on_launch = false
+  count = length(data.aws_availability_zones.azs.names)
+  subnet_az = data.aws_availability_zones.azs.names[count.index]
 }
