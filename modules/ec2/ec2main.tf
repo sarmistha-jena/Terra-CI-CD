5a3e625517645e28972650576913f7e048354240
Sarmistha Jena <sarmistha.jena@gmail.com>
2024-05-14 21:02:53 +0530

    added ig and rt

--- modules/ec2/ec2main.tf
+++ modules/ec2/ec2main.tf
@@ -5,7 +5,11 @@ resource "aws_instance" "ec2" {
   subnet_id                   = var.subnet
   instance_type               = var.instancetype
   key_name                    = var.keyname
-  user_data                   = var.userdata
+  user_data                   = <<-EOF
+  #!/bin/bash
+  echo '<html><body><h1 style="font-size:50px;color:blue;">Sarmistha <br> <font style="color:red;"> www.myweb.com <br> <font style="color:green;"> Learn Devops </h1> </body></html>' > index.html
+nohup busybox httpd -f -p 8080 &
+EOF
   tags                        = {
     Name = "Server ${var.build_name}"
   }
